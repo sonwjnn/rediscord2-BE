df1eab1657bc84e8e4f35c2dcec50565c43c97ba
sonwjnn <hoangsonj4f@gmail.com>
2025-06-01 11:13:40 +0700

    fix to deploy

--- src/module-alias.ts
+++ src/module-alias.ts
@@ -1,16 +1,8 @@
-/**
- * Register module aliases for path resolution
- */
 import moduleAlias from 'module-alias'
-import * as path from 'path'
+import { join } from 'path'
 
-// Different path resolution for development and production (Vercel)
-const rootDir =
-  process.env.NODE_ENV === 'production'
-    ? '/var/task'
-    : path.resolve(__dirname, '..')
+const baseDir = join(__dirname, '..')
 
-// Register the module aliases
 moduleAlias.addAliases({
-  '@': rootDir,
+  '@': baseDir,
 })
